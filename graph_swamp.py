e235876f9fe855ed6a1e89d72f6ad12916b6b5c9
dabuddahn <mark.sukhram@upr.edu>
2017-06-23 20:54:05 -0400

    Create graph_swamp.py

---
+++ graph_swamp.py
@@ -0,0 +1,38 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import Row, SparkSession
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+import numpy as np
+
+def sparksession(sparkConf):
+    if ('sparksessioninit' not in globals()):
+       globals()['sparksessioninit']=SparkSession.builder.config(conf=sparkConf).enableHiveSupport().getOrCreate()
+    return globals()['sparksessioninit']
+
+def graph():
+    spark = sparksession(spark_context.getConf())
+    time, date = sys.argv[1:]
+    interval_time = ("%s %s" %(date, time))
+    query = "select hashtag, sum(total) as global_total from hashtag_table \
+             where time_stamp between cast('{}' as timestamp) - INTERVAL 1 HOUR and cast('{}' as timestamp) \
+             group by hashtag order by global_total desc limit 10".format(interval_time, interval_time)
+    data = spark.sql(query)
+    data = data.filter("label='COMEY'").show()
+    data_format = data.toPandas()
+
+    fig1, ax1 = plt.subplots()
+
+    ax1.pie(data_format['global_total'], labels=data_format['hashtag'], autopct='%1.1f%%', shadow=True, startangle=135)
+
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    plt.show()
+
+if __name__ == "__main__":
+
+    print("Stating to plot graph") 
+
+    spark_context = SparkContext(appName="Graph of problem a")
+
+    graph()
